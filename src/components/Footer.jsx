f99d4604de0a98f41e0d4a26574447b390b37693
Lars Pieters <lars.pieters@student.howest.be>
2025-10-16 02:32:47 +0200

    updated footers with the right website link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ function Footer() {
         <div className='footer'>
             <img src={Bigline} alt="" className='full-width-img footer-line' />
             <ul>
-                <li><a href="www.lars-pieters.be" className='footer-link'><span className='gradient'>www</span>.lars-pieters.be</a></li>
+                <li><a href="www.smeshedup.studio" className='footer-link'><span className='gradient'>www</span>.smeshedup.studio</a></li>
                 <li><a href="mailto:larspieters@telenet.be" className='footer-link'>larspieters@telenet.be</a></li>
                 <li><a href="https://www.instagram.com/smeshedup/" className='footer-link'>Instagram</a></li>
             </ul>
--- src/components/FooterLight.jsx
+++ src/components/FooterLight.jsx
@@ -10,7 +10,7 @@ function Footer() {
         <div className='footer-light'>
             <img src={Bigline} alt="" className='full-width-img footer-line' />
             <ul>
-                <li><a href="www.lars-pieters.be" className='footer-link--light'><span className='gradient'>www</span>.lars-pieters.be</a></li>
+                <li><a href="www.smeshedup.studio" className='footer-link--light'><span className='gradient'>www</span>.smeshedup.studio</a></li>
                 <li><a href="mailto:larspieters@telenet.be" className='footer-link--light'>larspieters@telenet.be</a></li>
                 <li><a href="https://www.instagram.com/smeshedup/" className='footer-link--light'>Instagram</a></li>
             </ul>
